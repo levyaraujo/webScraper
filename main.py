bb1c0c8a6245024734f597d31b9d940a685d3012
Levy Araujo <levyxaraujo@gmail.com>
2022-05-29 13:39:19 -0300

    Reformat code

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import shutil
 import os
 import requests
-from scraper import getURL
+from scraper import get_url
 
 
-def saveFiles():  # esta função irá pegar os links da função anterior e salvar os arquivos em si
-    urls = getURL()
+def save_files():
+    """
+    Função que seleciona cada URL da lista retornada pela função get_url, baixa e salva os
+    arquivos PDF em uma pasta compactada
+    """
+    urls = get_url()
     folder = "PDFs"
     path = os.path.join(os.getcwd(), folder)
+    check = os.path.exists(
+        os.path.join(os.getcwd(), "PDFs.zip")
+    )  # checa se o arquivo já existe
 
+    if check:
+        print("\nO arquivo já existe no caminho especificado.\n")
+        return True
     try:
         os.mkdir(path, mode=0o666)
     except FileExistsError:
         print("A pasta que você tentou criar já existe")
+        return True
 
+    print("\nProcessando... Aguarde\n")
     for url in urls:
-        file_name_start = url.rfind("/") + 1
+        file_name_start = url.rfind("/") + 1  # extrai o nome do arquivo da URL
         file_name = url[file_name_start:]
         response = requests.get(url, stream=True)
 
@@ -24,15 +36,11 @@ def saveFiles():  # esta função irá pegar os links da função anterior e sal
                 new_file.write(response.content)
         else:
             response.raise_for_status()
-    print(f"Download finalizado. Arquivos salvos em \033[31m{path}\033[m")
 
-    anexo1 = os.path.join(
-        path, "Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536.pdf"
-    )
-
-    shutil.make_archive("PDFs", "zip", path)  # compress the folder
+    shutil.make_archive("PDFs", "zip", path)  # comprimindo os arquivos
     print("Pasta comprimida com sucesso!")
+    print(f"Download finalizado. Arquivos salvos em {path}.zip\n")
     shutil.rmtree(path)  # removendo pasta descompactada
 
 
-saveFiles()
+save_files()
